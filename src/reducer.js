5c1f4f7ebbe5cfa3f32f40d078dcde9b42ea28bb
sangkamath <twinkle_kamath@yahoo.com>
2024-02-17 19:40:26 +0530

    Add reducer case for todoAdded action

--- src/reducer.js
+++ src/reducer.js
@@ -10,8 +10,26 @@ const initialState = {
     }
 }
 
+function nextTodoId(todos) {
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+    return maxId + 1;
+}
+
 export default function appReducer(state = initialState, action) {
     switch(action.type) {
+        case "todos/todoAdded": {
+            return {
+                ...state,
+                todos: [
+                    ...state.todos,
+                    {
+                        id: nextTodoId(state.todos),
+                        text: action.payload,
+                        completed: false
+                    }
+                ]
+            }
+        }
         default:
             return state
     }
